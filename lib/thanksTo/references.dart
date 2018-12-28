c2205f22d26833cd6bd1796ac912478914f55b7f
stnieder <stefan.niederwanger@gmail.com>
2018-12-28 16:49:55 +0100

    Added reference page

---
+++ lib/thanksTo/references.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class References extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bilder aus dem Internet"),
+      ),
+      body: Column(
+        children: <Widget>[
+          GridView.builder(
+            itemCount: ,
+          )
+        ],
+      ),
+    );
+  }
+}
